edcf1ae3fe4511ae57ae4fb4a4ea69ada6b76bfe
juan miguel <jlojimeagu1@educacion.navarra.es>
2023-01-09 11:36:33 +0100

    save 1.0

---
+++ control_fichero.py
@@ -0,0 +1,13 @@
+# def lectura_escritura(escribe):
+# if input('decide entre 1 o 2') == 1:
+#    leer = open("Juan Miguel Jiménez Aguas - DGT", 'r')
+#    print(leer.readlines())
+# else:
+#    escribir = open("Juan Miguel Jiménez Aguas - DGT",'w')
+#    escribe = input('dime que escribo')
+#    escribir.write(escribe)
+
+countries_dict = {'30': 'Grecia', '33': 'Francia', '34': 'España', '351': 'Portugal', '380': 'Ucrania', '39': 'Italia',
+                  '41': 'Suiza', '44': 'Reino Unido', '49': 'Alemania', '7': 'Rusia'}
+
+nif_dict = {'0':'T', }
